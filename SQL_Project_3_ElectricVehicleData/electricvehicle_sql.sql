5ce19bd77b2ea5dc3a37cd97395232bf23a50faa
kinnari15 <kinbhalerao5@gmail.com>
2023-10-25 18:00:41 +0530

    New SQL Project

---
+++ SQL_Project_3_ElectricVehicleData/electricvehicle_sql.sql
@@ -0,0 +1,36 @@
+--Selecting the entire data and sorting by year
+
+SELECT *
+FROM portfolioproject..ElectricVehicleData
+--ORDER BY [Model Year] ASC
+
+--Selecting cars where model is a ranger
+
+SELECT Make, Model, [Model Year]
+FROM portfolioproject..ElectricVehicleData
+WHERE Model = 'Ranger'
+
+--Classifying CAFV Elibgibilty
+
+SELECT DISTINCT([Clean Alternative Fuel Vehicle (CAFV) Eligibility]), COUNT([Clean Alternative Fuel Vehicle (CAFV) Eligibility])
+FROM portfolioproject..ElectricVehicleData
+GROUP BY [Clean Alternative Fuel Vehicle (CAFV) Eligibility]
+Order by 1
+
+SELECT [Clean Alternative Fuel Vehicle (CAFV) Eligibility], Make
+, CASE WHEN [Clean Alternative Fuel Vehicle (CAFV) Eligibility] = 'Clean Alternative Fuel Vehicle Eligible' THEN 'Eligible'
+		WHEN [Clean Alternative Fuel Vehicle (CAFV) Eligibility] = 'Not eligible due to low battery range' THEN 'Not Eligible'
+		WHEN  [Clean Alternative Fuel Vehicle (CAFV) Eligibility] = 'Eligibility unknown as battery range has not been researched' THEN 'Eligiblity Unknown'
+		ELSE [Clean Alternative Fuel Vehicle (CAFV) Eligibility]
+		END
+FROM portfolioproject..ElectricVehicleData
+
+UPDATE portfolioproject..ElectricVehicleData
+SET [Clean Alternative Fuel Vehicle (CAFV) Eligibility] = CASE WHEN [Clean Alternative Fuel Vehicle (CAFV) Eligibility] = 'Clean Alternative Fuel Vehicle Eligible' THEN 'Eligible'
+		WHEN [Clean Alternative Fuel Vehicle (CAFV) Eligibility] = 'Not eligible due to low battery range' THEN 'Not Eligible'
+		WHEN  [Clean Alternative Fuel Vehicle (CAFV) Eligibility] = 'Eligibility unknown as battery range has not been researched' THEN 'Eligiblity Unknown'
+		ELSE  [Clean Alternative Fuel Vehicle (CAFV) Eligibility] 
+		END 
+
+
+
